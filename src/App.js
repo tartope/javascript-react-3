4f5282b68723093b4a43f748fce1ce1b79e345ff
Martin Burolla <mburolla@xpanxion.com>
2022-10-18 11:17:59 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
-import Button from 'react-bootstrap/Button';
 import React, { useState } from 'react';
+import Button from 'react-bootstrap/Button';
 import Offcanvas from 'react-bootstrap/Offcanvas';
 
 function App() {
   const [show, setShow] = useState(false);
   let [counter, setCounter] = useState(0);
-  let [showMenu, setShowMenu] = useState(false);
+  let [showFood, setShowFood] = useState(false);
+  let [showDrink, setShowDrink] = useState(false);
+  let [showDessert, setShowDessert] = useState(false);
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
@@ -15,37 +17,62 @@ function App() {
      setCounter(++counter)
   }
 
-  const onHandleLink = () => {
-    setShowMenu(!showMenu)
-    setShow(false)
+  const onHandleLink = (page) => {
+    setShowFood(false)
+    setShowDrink(false)
+    setShowDessert(false)
+    switch (page){
+      case "Food":
+        setShowFood(true)
+        break;
+      case "Drink":
+        setShowDrink(true)
+        break;
+      case "Dessert":
+        setShowDessert(true)
+        break;
+      default:
+    }
+    setShow(false) // Close the sidebar.
   }
 
   return (
     <div className="App">
-        <Button variant="primary" onClick={handleShow}>Sidebar</Button>
+        <Button variant="primary" onClick={handleShow}>Show Sidebar</Button>
         <br />
         <br />
         <Button variant="primary" onClick={() => onHandleClick()}>Inc Counter</Button>
         <br />
         {counter}
-
         <Offcanvas show={show} onHide={handleClose}>
           <Offcanvas.Header closeButton>
-            <Offcanvas.Title>Options</Offcanvas.Title>
+            <Offcanvas.Title>Sidebar</Offcanvas.Title>
           </Offcanvas.Header>
           <Offcanvas.Body>
-            <Button variant="primary" onClick={() => onHandleClick()}>Inc Counter</Button>
-            <br />
-            <Button variant="link" onClick={() => onHandleLink()}>Menu</Button>
+            <Button variant="primary" onClick={() => onHandleClick()}>Inc Counter</Button><br />
+            <Button variant="link" onClick={() => onHandleLink("Food")}>Food</Button><br />
+            <Button variant="link" onClick={() => onHandleLink("Drink")}>Drink</Button><br />
+            <Button variant="link" onClick={() => onHandleLink("Dessert")}>Dessert</Button><br />
           </Offcanvas.Body>
         </Offcanvas>
         {
-          showMenu &&
+          showFood &&
           <div>
-            Menu
+            Food
           </div>
         }
-      
+        {
+          showDrink &&
+          <div>
+            Drink
+          </div>
+        }
+        {
+          showDessert &&
+          <div>
+            Dessert
+          </div>
+        }
     </div>
   );
 }
