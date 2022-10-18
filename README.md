0aca67d23309d721ec153849795c32c11c69ff82
Martin Burolla <mburolla@xpanxion.com>
2022-10-18 11:02:59 -0400

    update

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ Simple project that shows a chart, grid and sidebar.
 # Getting Started
 - Clone this repo
 - Install dependencies: `npm install`
+
+# React Bootstrap
+https://react-bootstrap.github.io/getting-started/introduction/
--- src/App.js
+++ src/App.js
@@ -1,11 +1,51 @@
 import './App.css';
+import Button from 'react-bootstrap/Button';
+import React, { useState } from 'react';
+import Offcanvas from 'react-bootstrap/Offcanvas';
 
 function App() {
+  const [show, setShow] = useState(false);
+  let [counter, setCounter] = useState(0);
+  let [showMenu, setShowMenu] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const onHandleClick = () => {
+     setCounter(++counter)
+  }
+
+  const onHandleLink = () => {
+    setShowMenu(!showMenu)
+    setShow(false)
+  }
+
   return (
     <div className="App">
-        Hello World
-        <div>test</div>
-        <div>test</div>
+        <Button variant="primary" onClick={handleShow}>Sidebar</Button>
+        <br />
+        <br />
+        <Button variant="primary" onClick={() => onHandleClick()}>Inc Counter</Button>
+        <br />
+        {counter}
+
+        <Offcanvas show={show} onHide={handleClose}>
+          <Offcanvas.Header closeButton>
+            <Offcanvas.Title>Options</Offcanvas.Title>
+          </Offcanvas.Header>
+          <Offcanvas.Body>
+            <Button variant="primary" onClick={() => onHandleClick()}>Inc Counter</Button>
+            <br />
+            <Button variant="link" onClick={() => onHandleLink()}>Menu</Button>
+          </Offcanvas.Body>
+        </Offcanvas>
+        {
+          showMenu &&
+          <div>
+            Menu
+          </div>
+        }
+      
     </div>
   );
 }
