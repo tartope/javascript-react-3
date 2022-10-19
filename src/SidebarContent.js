119bc13a522273628d3a633eb5a8a7e4286f6855
Martin Burolla <mburolla@xpanxion.com>
2022-10-19 11:31:19 -0400

    update

--- src/SidebarContent.js
+++ src/SidebarContent.js
@@ -15,7 +15,7 @@ export const SidebarContent = ({onHandleLink, onHandleCheckout}) => {
 
     // Convert drink object to array for display.
     let d = Object.keys(drinks)
-    let array = d.map(i => {
+    let purchasedItems = d.map(i => {
         return {
             type: i,
             qty: drinks[i]
@@ -23,7 +23,7 @@ export const SidebarContent = ({onHandleLink, onHandleCheckout}) => {
     })
 
     // Calculate total.
-    let priceArray = array.map(i => {
+    let priceArray = purchasedItems.map(i => {
         return i.qty * getPriceForDrink(i.type)
     })
     grandTotal = priceArray.reduce((a,b) => a + b, 0)
@@ -59,22 +59,47 @@ export const SidebarContent = ({onHandleLink, onHandleCheckout}) => {
                 grandTotal > 0 &&
                 <strong>Drinks</strong>
             }
-            { 
-                array.map(i => {
-                    if (i.qty > 0) {
-                        return <div key={i.type}>{i.type}: {i.qty} x { formatter.format(getPriceForDrink(i.type)) }</div>
-                    } else
-                        return null
-                })
-            }
+            {
+                 grandTotal > 0 &&
+                <table className='SidebarContent_Table'>
+                    <thead>
+                        <tr>
+                            <td>Item</td>
+                            <td>Qty</td>
+                            <td>Price</td>
+                            <td>Subtotal</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        { 
+                            purchasedItems.map(i => {
+                                if (i.qty > 0) {
+                                    return <tr key={i.type}>
+                                                <td >{i.type}</td>
+                                                <td>{i.qty}</td>
+                                                <td>{ formatter.format(getPriceForDrink(i.type)) } {' '}ea</td>
+                                                <td>{formatter.format(getPriceForDrink(i.type) * i.qty)}</td>
+                                            </tr>
+                                } else {
+                                    return null
+                                }
+                            })
+                        }
+                    </tbody>
+                </table>  
+            } 
             {
                 grandTotal === 0 &&
                 <div>Your cart is empty.</div>
             }
             <hr />
-            Your Total: { formatter.format(grandTotal)}
+            <div className='SidebarContent_Total'>
+                Your Total:<span>{ formatter.format(grandTotal)}</span>
+            </div>
             <hr />
-            <Button variant="primary" onClick={() => onHandleCheckout(grandTotal)}>Checkout</Button>
+            <div className='SidebarContent_Checkout'>
+                <Button variant="primary" onClick={() => onHandleCheckout(grandTotal)}>Checkout</Button>
+            </div>
         </div>
     )
 }
