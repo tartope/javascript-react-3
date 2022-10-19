5b316efe6c63eac382a467e638634717677186a7
Martin Burolla <mburolla@xpanxion.com>
2022-10-18 21:54:04 -0400

    update

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Footer } from '../Footer'
 import { CoffeeCarousel } from '../CoffeeCarousel'
+import Image from 'react-bootstrap/Image'
 
 import './HomePage.scss'
 
@@ -15,7 +16,10 @@ export const HomePage = () => {
         <div className='HomePage_Header'>
             Live Music Every Friday Night
         </div>
-        
+        <div className='HomePage_Band'>
+            <Image width="800px" rounded={true} fluid={true} src="band.jpg" alt="band" />
+        </div>
+        <br /><br />
         <Footer />
     </div>
   )
