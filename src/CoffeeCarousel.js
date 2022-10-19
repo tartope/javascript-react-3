14e8e6302686458b43a058ed66857fb066e90b96
Martin Burolla <mburolla@xpanxion.com>
2022-10-19 13:07:01 -0400

    update

--- src/CoffeeCarousel.js
+++ src/CoffeeCarousel.js
@@ -5,40 +5,24 @@ export const CoffeeCarousel = () => {
   return (
     <Carousel>
       <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="coffee-shop-3.jpg"
-          alt="First slide"
-        />
+        <img className="d-block w-100" src="coffee-shop-3.jpg" alt="First slide"/>
         <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          {/* <h3>Free Cookies Every Monday!</h3> */}
+          {/* <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p> */}
         </Carousel.Caption>
       </Carousel.Item>
-
       <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="coffee-shop-2.jpg"
-          alt="Second slide"
-        />
+        <img className="d-block w-100" src="coffee-shop-2.jpg" alt="Second slide" />
         <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          {/* <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p> */}
         </Carousel.Caption>
       </Carousel.Item>
-      
       <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="coffee-shop-3.jpg"
-          alt="Third slide"
-        />
+        <img className="d-block w-100" src="coffee-shop-3.jpg" alt="Third slide"/>
         <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
+          {/* <h3>Third slide label</h3>
+          <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p> */}
         </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
