dcc8e706ec87a458425c7ad2a40477b83936c4f1
Martin Burolla <mburolla@xpanxion.com>
2022-10-18 23:38:04 -0400

    update

--- src/App.js
+++ src/App.js
@@ -8,9 +8,9 @@ import Offcanvas from 'react-bootstrap/Offcanvas'
 import './App.scss'
 
 function App() {
-  let [showHome, setShowHome] = useState(false);
+  let [showHome, setShowHome] = useState(true);
   let [showFood, setShowFood] = useState(false);
-  let [showDrink, setShowDrink] = useState(true);
+  let [showDrink, setShowDrink] = useState(false);
   let [showSales, setShowSales] = useState(false);
   let [showDessert, setShowDessert] = useState(false);
   let [showTheSideDrawer, setShowSideDrawer] = useRecoilState(showSideDrawer);
