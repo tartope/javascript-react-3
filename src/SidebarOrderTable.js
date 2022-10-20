c8885f57d345d7a26f9b582275b97290ba4399d9
Martin Burolla <mburolla@xpanxion.com>
2022-10-20 00:33:25 -0400

    update

--- src/SidebarOrderTable.js
+++ src/SidebarOrderTable.js
@@ -3,6 +3,10 @@ import './SidebarOrderTable.scss'
 
 export const SidebarOrderTable = ({completeOrder}) => {
     const formatter = new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' });
+    const capitalize = (s) => {
+        if (typeof s !== 'string') return ''
+        return s.charAt(0).toUpperCase() + s.slice(1)
+    }
 
     return (
         <div className='SidebarOrderTable'>
@@ -19,7 +23,7 @@ export const SidebarOrderTable = ({completeOrder}) => {
                     { 
                         completeOrder.map(i => {
                             return <tr key={ i.item }>
-                                        <td>{ i.item }</td>
+                                        <td>{ capitalize(i.item) }</td>
                                         <td>{ i.qty }</td>
                                         <td>{ formatter.format(i.price) } {' '}ea</td>
                                         <td>{ formatter.format(i.subTotal) }</td>
