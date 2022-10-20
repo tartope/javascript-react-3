d59e469b14a2ff549d61988bb98183c2e51a94cc
Martin Burolla <mburolla@xpanxion.com>
2022-10-20 13:20:16 -0400

    update

--- src/SidebarContent.js
+++ src/SidebarContent.js
@@ -9,8 +9,6 @@ import { OrderSection } from './OrderSection'
 export const SidebarContent = ({onHandleLink, onHandleCheckout}) => {
     const {order, total} = buildOrder(useRecoilValue(orderedDrinks)) 
 
-    console.log(order)
-    
     return (
         <div className='SidebarContent'>
             <SidebarMenu onHandleLink={(page) => onHandleLink(page)}/>
