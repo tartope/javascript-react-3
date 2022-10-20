dc08d97feaaa700376bfc0be957e767f70eea359
Martin Burolla <mburolla@xpanxion.com>
2022-10-20 01:01:23 -0400

    update

--- src/Header.js
+++ src/Header.js
@@ -6,10 +6,11 @@ import './Header.scss'
 
 export const Header = () => {
     const [, setShowSideDrawer] = useRecoilState(showSideDrawer)
-    let theDrinkData = useRecoilValue(drinkData) 
+    const drinks = useRecoilValue(drinkData) 
 
     const getQuantity = () => {
-        return parseInt(theDrinkData.tea) + parseInt(theDrinkData.coffee) + parseInt(theDrinkData.lemonade)
+        let totalDrinks = (drinks.tea + drinks.coffee + drinks.lemonade)
+        return totalDrinks
     }
 
     const getClassName = () => {
@@ -22,12 +23,12 @@ export const Header = () => {
 
     return (
         <div className="Header">
-            <div>
-                <img className="Header_hamburger" onClick={() => setShowSideDrawer(true)} src="hamburger.png" alt="menu"></img>
-                </div>
-                <div className="Header_logo_area">
-                <div className="Header_logo">The Coffee Place</div>
-                <div className="Header_cart" onClick={() => setShowSideDrawer(true)}>
+            <div className="Header_Hamburger">
+                <img onClick={() => setShowSideDrawer(true)} height="40px" width="40px" src="hamburger.png" alt="menu"></img>
+            </div>
+            <div className="Header_Logo_Area">
+                <div className="Header_Logo">The Coffee Place</div>
+                <div className="Header_Cart" onClick={() => setShowSideDrawer(true)}>
                     <img width="40px" src="shopping-cart.png" alt="shopping cart"/>
                     <div className={getClassName()}>{getQuantity()}</div>
                 </div>
