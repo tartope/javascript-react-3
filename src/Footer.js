afde1276a309b921d10fe1272520aeefa208e68a
Martin Burolla <mburolla@xpanxion.com>
2022-10-19 23:40:48 -0400

    update

--- src/Footer.js
+++ src/Footer.js
@@ -5,15 +5,15 @@ import './Footer.scss'
 export const Footer = () => {
   return (
     <div className='Footer'>
-        <div className='menu'>
+        <div className='Footer_menu'>
             Careers <br/>
             About Us<br/>
             Privacy <br/>
         </div>
-        <div className='center'>
+        <div className='Footer_center'>
             Copyright 2022
         </div>
-        <div className='social'>
+        <div className='Footer_social'>
             <Image className='icon' width="25px" rounded={true} fluid={true} src="insta-icon.png" alt="insta" />
             <Image className='icon' width="25px" rounded={true} fluid={true} src="facebook-icon.png" alt="facebook" />
             <Image className='icon' width="25px" rounded={true} fluid={true} src="twitter-icon.png" alt="twitter" />
