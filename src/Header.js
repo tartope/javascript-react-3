97bb587ccd25b6df1ed37910ae65f613eb694c28
Martin Burolla <mburolla@xpanxion.com>
2022-10-20 22:02:30 -0400

    update

--- src/Header.js
+++ src/Header.js
@@ -9,28 +9,30 @@ export const Header = () => {
     const drinks = useRecoilValue(orderedDrinks) 
 
     const getQuantity = () => {
-        const totalDrinks = (drinks.tea + drinks.coffee + drinks.lemonade)
-        return totalDrinks
+        return (drinks.tea + drinks.coffee + drinks.lemonade)
     }
 
     const getClassName = () => {
-        // Use a different class for larger orders.
-        if (getQuantity() < 10) {
-          return "Header_qty_1"
-        } else {
-          return "Header_qty_2" 
-        }
+        return (getQuantity() < 10) ? 'Header_qty_1' : 'Header_qty_2' 
     }
 
     return (
-        <div className="Header">
-            <div className="Header_Hamburger">
-                <img onClick={() => setShowSideDrawer(true)} height="40px" width="40px" src="hamburger.png" alt="menu"></img>
+        <div className='Header'>
+            <div className='Header_Hamburger'>
+                <img 
+                    onClick={() => setShowSideDrawer(true)} 
+                    height='40px' 
+                    width='40px' 
+                    src='hamburger.png' 
+                    alt='menu'>
+                </img>
             </div>
-            <div className="Header_Logo_Area">
-                <div className="Header_Logo">The Coffee Place</div>
-                <div className="Header_Cart" onClick={() => setShowSideDrawer(true)}>
-                    <img width="40px" src="shopping-cart.png" alt="shopping cart"/>
+            <div className='Header_Logo_Area'>
+                <div className='Header_Logo'>
+                    The Coffee Place
+                </div>
+                <div className='Header_Cart' onClick={() => setShowSideDrawer(true)}>
+                    <img width='40px' src='shopping-cart.png' alt='shopping cart'/>
                     <div className={getClassName()}>{getQuantity()}</div>
                 </div>
             </div>
