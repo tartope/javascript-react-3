c82753a1e5ca1f013187b389923bec934bdee39a
Martin Burolla <mburolla@xpanxion.com>
2022-10-22 21:38:07 -0400

    update

--- devops/build-deploy.bat
+++ devops/build-deploy.bat
@@ -1,4 +1,4 @@
 call npm run build
-call aws s3 sync ../build s3://coffee-place
+call aws s3 sync ../build s3://my-coffee-place
 
-echo http://coffee-place.s3-website-us-east-1.amazonaws.com
+echo http://my-coffee-place.s3-website-us-east-1.amazonaws.com
